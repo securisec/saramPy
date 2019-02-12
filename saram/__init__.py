036c0107c6938203e4e892ac00927c98f24b748e
josh @securisec <josh@securisec.com>
2019-02-11 19:52:56 -0500

    some refactors and added create_entry method

--- saram/__init__.py
+++ saram/__init__.py
@@ -4,6 +4,7 @@ import re
 import json
 import requests
 import delegator
+from uuid import uuid1
 
 __version__ = 0.1
 __author__ = 'Hapsida @securisec'
@@ -30,16 +31,21 @@ class Saram(object):
         self.slack_user = slack_user
         self.type = None
         self.token = token
-        self.url =  self.__check_dev(self.token)
+        self.url = self._check_dev(self.token)
         print(self.url)
 
-    def __check_dev(self, token):
-        if os.environ.get('PARAMDUNI_ENV') == 'dev':
-            return f'http://localhost:5001/{token}'
+    def _check_dev(self, token, base_url: bool=False) -> str:
+        if os.environ.get('SARAM_ENV') == 'dev':
+            return 'http://localhost:5001/' if base_url else f'http://localhost:5001/{token}'
         else:
-            return f'https://saram.securisec.com/{token}'
+            return 'https://saram.securisec.com/' if base_url else f'https://saram.securisec.com/{token}'
 
-    def read_script(self) -> 'Saram':
+    def _token_generator(self, title: str) -> str:
+        u = str(uuid1())[0:8]
+        t = '-'.join(re.sub(r'[^a-zA-Z0-9 ]', '', title).split())[0:25]
+        return f'{u}-{t}'
+
+    def read_script(self, script_name: str=None) -> 'Saram':
         '''
         Read the contents of the file that this function is 
         called in and return the whole content
@@ -54,15 +60,17 @@ class Saram(object):
             self.file_path = path
             self.output = data
             self.type = 'script'
-            self.command_run = 'Script'
+            self.command_run = 'Script' if script_name is None else script_name
             return self
 
-    def file_content(self, file_path: str) -> 'Saram':
+    def file_content(self, file_path: str, file_name: str=None) -> 'Saram':
         '''
         Read a files content
 
         :param file_path: File path
         :type file_path: str
+        :param file_name: File path
+        :type file_name: str
         :return: Saram object
         :rtype: object
         '''
@@ -72,22 +80,24 @@ class Saram(object):
             self.file_path = file_path
             self.output = data
             self.type = 'file'
-            self.command_run = 'File'
+            self.command_run = 'File' if file_name is None else file_name
             return self
 
-    def script_output(self, var: any) -> 'Saram':
+    def variable_output(self, var: any, script_name: str=None) -> 'Saram':
         '''
         Send any data like the output of a python script 
         to the server
 
         :param var: Variable
         :type var: any
+        :param script_name: Variable
+        :type script_name: any
         :return: Saram object
         :rtype: object
         '''
 
         self.type = 'script'
-        self.command_run = 'Script'
+        self.command_run = 'Script' if script_name is None else script_name
         self.output = var
         return self
 
@@ -100,6 +110,7 @@ class Saram(object):
         '''
 
         json = {
+            'id': str(uuid1()),
             'type': self.type,
             'output': self.output,
             'command': self.command_run,
@@ -142,3 +153,27 @@ class Saram(object):
         self.command_error = output.err
         self.command_run = command
         return self
+
+    def create_entry(self, title: str, category: str,
+                     slack_link: str, create_token: str) -> 'Saram':
+        entry = {
+            'title': title,
+            'category': category,
+            'slackLink': slack_link,
+            'data': []
+        }
+        header = {
+            'x-saram': create_token
+        }
+        token = self._token_generator(title)
+        base_url = self._check_dev(None, True)
+        url = f'{base_url}create/{token}'
+        entry_url = f'{base_url}{token}'
+        r = requests.post(url, json=entry, headers=header)
+        print(r.status_code)
+        print(entry_url)
+        self.response = r
+        self.url = url
+        self.token = token
+        return self
+        
--- saram/__main__.py
+++ saram/__main__.py
@@ -6,6 +6,7 @@ def main():
     parse = argparse.ArgumentParser()
     parse.add_argument('-t', dest='token', required=True, help='Token provided in Slack')
     parse.add_argument('-u', dest='slack_user', required=True, help='Slack username')
+    parse.add_argument('-n', dest='name', default=None, help='Name of the file')
     
     group = parse.add_mutually_exclusive_group()
     group.add_argument('-c', dest='command', help='Command to run inside quotes')
@@ -18,7 +19,7 @@ def main():
     if args.command:
         p.run_command(args.command).send_to_server()
     elif args.file:
-        p.file_content(args.file).send_to_server()
+        p.file_content(args.file, file_name=args.name).send_to_server()
 
 
 if __name__ == "__main__":
