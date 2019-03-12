988582a66f647b5afabbb84604677c77bb08c269
securisec <securisecctf@gmail.com>
2019-03-12 17:08:27 +0900

    travis integration

---
+++ test/travis.py
@@ -0,0 +1,21 @@
+from saram import Saram, SaramHelpers
+from pprint import pprint
+
+def dump(obj):
+  for attr in dir(obj):
+    print("obj.%s = %r" % (attr, getattr(obj, attr)))
+
+s = Saram(
+    token='Some Token',
+    user='SomeUser',
+    local=True
+)
+
+test = 'Test'
+
+dump(s)
+
+s.run_command('ls')
+s.script_read_self(comment='comment')
+s.script_dump(comment='comment')
+s.variable_output(test, comment='comment')
